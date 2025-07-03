372ac5e98f01ab60dbd29efdcbfae054fb71da75
vvinod <vinod@uni-wuppertal.de>
2025-07-03 10:42:19 +0200

    new webpage design

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+
+particlesJS('particles-js', {
+    particles: {
+        number: {
+            value: 80
+        },
+        size: {
+            value: 3
+        },
+        color: {
+            value: "#ffffff"
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 3
+        }
+    }
+});
