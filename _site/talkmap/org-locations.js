127a928a44446603f39dba50329c84f61394ed77
vvinod <vinod@uni-wuppertal.de>
2025-11-14 17:52:16 +0100

    minor format changes to talk map.

---
+++ _site/talkmap/org-locations.js
@@ -0,0 +1,62 @@
+var addressPoints = [
+  [
+    "Optimized Multi-Fidelity Machine Learning for Quantum Chemistry.<br />SPP-2363 Annual Meet; J\u00e4gerberg 1, 06108 Halle (Saale)",
+    51.4872888,
+    11.9629364
+  ],
+  [
+    "Multi-Fidelity Machine Learning for Excited State Energies.<br />Machine Learning and Quantum Computing for Quantum Molecular Dynamics - MLQCDyn; Universit\u00e9 Gustave Eiffel, 5 Boulevard Descartes, 77420 Champs-sur-Marne, France",
+    48.8392091,
+    2.5875153
+  ],
+  [
+    "Small area estimation methods for NUTS3 level poverty estimation in Italy.<br />Small Area Estimation Method Seminar, Scuola S\u2019ant Anna; P.zza Martiri della Libert\u00e0, 33 - 56127 - Pisa (Italy)",
+    43.7206078,
+    10.4026444
+  ],
+  [
+    "Optimized Multi-Fidelity Machine Learning for Quantum Chemical Properties.<br />Fritz Haber Institute, Theory Department, Berlin; Zuse Institute Berlin, Takustra\u00dfe 7 14195 Berlin Germany",
+    52.4557415,
+    13.2979865
+  ],
+  [
+    "Optimized Multi-Fidelity Machine Learning for Quantum Chemistry.<br />Young Chemists Summit (Salzburg); Paris Lodron University Salzburg, Salzburg, Austria",
+    47.7984448,
+    13.0503867
+  ],
+  [
+    "Optimal Combination and Data Hierarchies in Multifidelity Machine Learning<br />MESIGA25: Numerical Methods in Applied Mathematics, University of Potsdam; Karl-Liebknecht-Stra\u00dfe 24 - 25, 14476 Potsdam, Germany",
+    52.4090285,
+    12.9730604
+  ],
+  [
+    "Chua diode circuit as a secure communication device using chaos theory.<br />St. Stephen's College: Meera Memorial Paper Presentation; St. Stephen's College, University Enclave, North Campus, Delhi  - 110007, India",
+    28.6860857,
+    77.2114567
+  ],
+  [
+    "Delauney Triangulation for Voronoi Partition for implementation in Simulated Mobile Network Data.<br />International Conference Use of R in Official Statistics (uRosConf 2021); Bucharest, Romania",
+    44.4361414,
+    26.1027202
+  ],
+  [
+    "Multifidelity Machine Learning and Active Learning Strategies for Quantum Chemistry.<br />SPP-2363 Annual Meet; J\u00e4gerberg 1, 06108 Halle (Saale)",
+    51.4872888,
+    11.9629364
+  ],
+  [
+    "Multi-Fidelity Machine Learning for Excited State Energies.<br />SIMPLAIX  Workshop on Machine Learning for Multi-scale Molecular Modeling; Schloss-Wolfsbrunnenweg 35, 69118 Heidelberg, Germany",
+    49.4153802,
+    8.7365752
+  ],
+  [
+    "simviz: a package to visualize simulated telecommunication mobile network event data.<br />International Conference Use of R in Official Statistics (uRosConf 2021); Bucharest, Romania",
+    44.4361414,
+    26.1027202
+  ],
+  [
+    "Living conditions and Poverty in Italy - 2017. Small Area estimation and machine learning methods.<br />University of Pisa: Erasmus+ & Centro Camilo Dagum & Jean Monnet Chair EU; Via Cosimo Ridolfi 10 \u2013 56124 Pisa",
+    43.7113533,
+    10.4105646
+  ]
+];
